11fc568e345c3f81e268cc6d68aecb6283a53994
itamm15 <osinski.mateusz15@gmail.com>
2022-12-28 23:35:27 +0100

    delete action

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,31 +5,71 @@ const table = {
   width: "90%",
 };
 
+const actions = {
+  display: "flex",
+  justifyContent: "center",
+  columnGap: "8%",
+};
+
+const tableHeaderAndBody = {
+  textAlign: "center",
+};
+
 const ExpenseList = () => {
-  const { expensesList, error, isLoading } = useExpenses();
+  const { expensesList, setExpensesList, error, isLoading } = useExpenses();
+
+  async function handleDelete(event, expenseId) {
+    event.preventDefault();
+
+    await fetch(`http://localhost:3001/expenses/${expenseId}`, {
+      method: "DELETE",
+      headers: { "Content-type": "application/json" },
+    }).then(() => {
+      const filteredExpenses = expensesList.filter(({ id }) => {
+        return expenseId !== id;
+      });
+      setExpensesList(filteredExpenses);
+    });
+  }
+
   return (
     <div style={table}>
       {error && <h1>Could not fetch data, an error occurred.</h1>}
       {!isLoading && (
         <table className="table">
           <thead>
-            <tr>
+            <tr style={tableHeaderAndBody}>
               <th scope="col">Amount</th>
               <th scope="col">Description</th>
               <th scope="col">Date</th>
               <th scope="col">Type</th>
+              <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
             {expensesList &&
-              expensesList.map(({ amount, description, expenseType, date }) => (
-                <tr>
-                  <th scope="row">{amount}</th>
-                  <th>{description}</th>
-                  <th>{date}</th>
-                  <th>{expenseType}</th>
-                </tr>
-              ))}
+              expensesList.map(
+                ({ id, amount, description, expenseType, date }) => (
+                  <tr style={tableHeaderAndBody}>
+                    <th scope="row">{amount}</th>
+                    <th>{description}</th>
+                    <th>{date}</th>
+                    <th>{expenseType}</th>
+                    <th style={actions}>
+                      <button type="button" className="btn btn-warning btn-sm">
+                        Update
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-danger btn-sm"
+                        onClick={(event) => handleDelete(event, id)}
+                      >
+                        Delete
+                      </button>
+                    </th>
+                  </tr>
+                )
+              )}
           </tbody>
         </table>
       )}
