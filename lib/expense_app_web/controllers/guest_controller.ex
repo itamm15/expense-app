e73e1d17ea1e726eba00b2ddba9a82aa1fe5e17b
itamm15 <osinski.mateusz15@gmail.com>
2023-06-26 23:42:17 +0200

    :hammer: put_status in guest_controller

--- lib/expense_app_web/controllers/guest_controller.ex
+++ lib/expense_app_web/controllers/guest_controller.ex
@@ -7,10 +7,13 @@ defmodule ExpenseAppWeb.GuestController do
   def create(conn, params) do
     case Guest.create_user(params) do
       {:ok, created_user} ->
-        json(conn, created_user)
+        conn
+        |> put_status(201)
+        |> json(created_user)
 
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
+        |> put_status(400)
         |> put_view(ExpenseAppWeb.ErrorView)
         |> render("error.json", changeset: changeset)
     end
