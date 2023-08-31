3e72bd0c3f819371e3e17ba280ed832dfe8371fa
olafbado <niebiel1@gmail.com>
2023-08-31 19:19:08 +0200

    refactor: :recycle: Update assessment context

--- lib/expense_app/assessment.ex
+++ lib/expense_app/assessment.ex
@@ -3,12 +3,10 @@ defmodule ExpenseApp.Assessment do
   Module responsible for handling expenses.
   """
 
+  import Contexted.Delegator
+
   alias ExpenseApp.Context.Expenses
 
-  defdelegate get_expense(id), to: Expenses
-  defdelegate get_expenses_for_user(user_id), to: Expenses
-  defdelegate list_expenses, to: Expenses
-  defdelegate create_expense(expense), to: Expenses
-  defdelegate update_expense(expense, params), to: Expenses
-  defdelegate delete_expense(expense), to: Expenses
+  delegate_all(Expenses)
+
 end
--- lib/expense_app/assessment/context/expenses.ex
+++ lib/expense_app/assessment/context/expenses.ex
@@ -1,57 +1,20 @@
 defmodule ExpenseApp.Context.Expenses do
   @moduledoc false
 
+  use Contexted.CRUD,
+    repo: ExpenseApp.Repo,
+    schema: ExpenseApp.Expense
+
   import Ecto.Query
 
   alias ExpenseApp.Expense
   alias ExpenseApp.Repo
   alias ExpenseApp.User
 
-  @spec get_expense(Expense.id()) :: Expense.t()
-  def get_expense(id) do
-    Expense
-    |> where([expense], expense.id == ^id)
-    |> Repo.one()
-  end
-
   @spec get_expenses_for_user(User.id()) :: [Expense.t()]
   def get_expenses_for_user(user_id) do
     Expense
     |> where([expense], expense.user_id == ^user_id)
     |> Repo.all()
-  end
-
-  @spec list_expenses() :: [Expense.t()]
-  def list_expenses do
-    Expense
-    |> Repo.all()
-  end
-
-  @spec create_expense(map()) :: {:ok, Expense.t()} | {:error, Ecto.Changeset.t()}
-  def create_expense(expense) do
-    %Expense{}
-    |> Expense.changeset(expense)
-    |> Repo.insert()
-  end
-
-  @spec update_expense(Expense.t(), map()) :: {:ok, Expense.t()} | {:error, Ecto.Changeset.t()}
-  def update_expense(%Expense{} = expense, params) do
-    expense
-    |> Expense.changeset(params)
-    |> Repo.update()
-  end
-
-  @spec delete_expense(Expense.t() | Expense.id()) ::
-          {:ok, Expense.t()} | {:error, Ecto.Changeset.t()}
-  def delete_expense(id) when is_binary(id) do
-    id
-    |> String.to_integer()
-    |> get_expense()
-    |> delete_expense()
-  end
-
-  def delete_expense(expense) do
-    expense
-    |> Repo.delete()
   end
 end
