7c90b0938a47d534fdce559856e19637ed542190
itamm15 <osinski.mateusz15@gmail.com>
2023-09-02 20:57:46 +0200

    :sparkles: base for login page

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,7 +1,32 @@
+import "../styles/Login.scss";
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="login">
+      <div className="login__form">
+        <div className="login__form-title">
+          <h1>Login</h1>
+        </div>
+        <div className="login__form-input">
+          <input
+            type="email"
+            placeholder="email"
+            className="login--input"
+            required
+          />
+        </div>
+        <div className="login__form-input">
+          <input
+            type="password"
+            placeholder="password"
+            className="login--input"
+            required
+          />
+        </div>
+        <div className="login__form-submit">
+          <button type="submit">Login</button>
+        </div>
+      </div>
     </div>
   );
 };
